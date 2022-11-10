45d72517c3348724ece21f235decfcfc799dac64
Vinh Tho Hoang <vinhtho.hoang@gmail.com>
2022-11-10 09:08:42 -0500

    restaurant

---
+++ app/models/restaurant.rb
@@ -0,0 +1,7 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: destroy
+
+  CATEGORIES = %w[chinese italian japanese french belgian]
+
+  validates :categories, inclusion: { in: CATEGORIES }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # List all restaurants (index)
+  # get 'restaurants', to: 'restaurants#index'
+  # # Add a new restaurant
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+  # # A user can see the details and all reviews of a specific restaurant
+  # get 'restaurants/:id', to: 'restaurants#show'
+  # # A user can add a new review to a restaurant
+  # get 'restaurants/:restaurant_id/reviews/new', to: 'reviews#new', as: :new_review
+  # post 'restaurants/:restaurant_id/reviews', to: 'reviews#create', as: :restaurant_review
+
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:new, :create]
+  end
 end
